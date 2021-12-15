ff7be65502c6ac24212fcd04cbd9fef6387a5d63
fa0311 <yukinashi2742756@yahoo.co.jp>
2021-12-16 05:43:12 +0900

    add tags search

--- getAppCubes/getAppCubes.py
+++ getAppCubes/getAppCubes.py
@@ -7,18 +7,26 @@ class getAppCubes:
         self.HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
         self.PROXIES = {}
         self.BASE_URL = 'https://apps.moons14.com/api/developer/'
+        self.APP_BASE_URL = 'https://apps.moons14.com/apps/'
+        self.TIMEZONE = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
 
     def apps(self, count = 30, loginonly = False):
         endpoint = 'apps'
         params =  {'count': count, 'loginonly': loginonly}
         response = self.__send_requests(endpoint, params)
-        return [AppCubesApp(**app) for app in response['apps']]
+        return [AppCubesApp(AppCubes=self, **app) for app in response['apps']]
     
     def search(self, query, count = 30, loginonly = False):
         endpoint = 'search'
         params =  {'q': query, 'count': count, 'loginonly': loginonly}
         response = self.__send_requests(endpoint, params)
-        return [AppCubesApp(**app) for app in response['apps']]
+        return [AppCubesApp(AppCubes=self, **app) for app in response['apps']]
+
+    def tags(self, query, count = 30, loginonly = False):
+        endpoint = 'tags'
+        params =  {'tags': query, 'count': count, 'loginonly': loginonly}
+        response = self.__send_requests(endpoint, params)
+        return [AppCubesApp(AppCubes=self, **app) for app in response['apps']]
 
     def __send_requests(self, endpoint, params):
         response = requests.get(self.BASE_URL + endpoint, params=params, headers=self.HEADERS, proxies=self.PROXIES)
@@ -30,6 +38,7 @@ class getAppCubes:
 
 @dataclass
 class AppCubesApp:
+    AppCubes:object = None
     id: str = None
     loginonly: str = None
     type: str = None
@@ -38,19 +47,26 @@ class AppCubesApp:
     updated_at: str = None
 
     def __post_init__(self):
-        TIMEZONE = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
-        BASEURL = 'https://apps.moons14.com/apps/'
-        self.url = BASEURL + self.id
+        self.url = self.AppCubes.APP_BASE_URL + self.id
         self.loginonly = bool(self.loginonly)
-        self.updated_at = datetime.datetime.fromtimestamp(int(self.updated_at), TIMEZONE)
-        self.jp = AppCubesAppLang(**self.jp)
-        self.en = AppCubesAppLang(**self.en)
+        self.updated_at = datetime.datetime.fromtimestamp(int(self.updated_at), self.AppCubes.TIMEZONE)
+        self.jp = AppCubesAppLang(AppCubes=self.AppCubes, **self.jp)
+        self.en = AppCubesAppLang(AppCubes=self.AppCubes, **self.en)
 
 @dataclass
 class AppCubesAppLang:
+    AppCubes:object = None
     name: str = None
     explanation: str = None
     tags: str = None
 
     def __post_init__(self):
-        self.tags = self.tags.split(',')
+        self.tags = [AppCubesTag(AppCubes=self.AppCubes, name=tag) for tag in self.tags.split(',')]
+        
+@dataclass
+class AppCubesTag:
+    AppCubes:object = None
+    name: str = None
+    
+    def search(self, count = 30, loginonly = False):
+        return self.AppCubes.tags(query=self.name, count=count, loginonly=loginonly)
--- sample.py
+++ sample.py
@@ -8,17 +8,17 @@ if __name__ == '__main__':
     print('Enter the app name')
     query = input()
     AppCubes = getAppCubes.getAppCubes().search(query=query, count=30, loginonly=False)
+    
+    if len(AppCubes) > 0:
+        dump('id', AppCubes[0].id)
+        dump('url', AppCubes[0].url)
+        dump('loginonly', AppCubes[0].loginonly)
+        dump('type', AppCubes[0].type)
+        dump('updated_at', AppCubes[0].updated_at)
+        dump('jp.name', AppCubes[0].jp.name)
+        dump('jp.explanation', AppCubes[0].jp.explanation)
+        dump('jp.tags', AppCubes[0].jp.tags[0].name)
+    else:
+        print('Not found')
 
-    dump('id', AppCubes[0].id)
-    dump('url', AppCubes[0].url)
-    dump('loginonly', AppCubes[0].loginonly)
-    dump('type', AppCubes[0].type)
-    dump('updated_at', AppCubes[0].updated_at)
-
-    dump('jp.name', AppCubes[0].jp.name)
-    dump('jp.explanation', AppCubes[0].jp.explanation)
-    dump('jp.tags', AppCubes[0].jp.tags)
-    
-    
-    
     AllAppCubes = getAppCubes.getAppCubes().apps(count=30, loginonly=False)
